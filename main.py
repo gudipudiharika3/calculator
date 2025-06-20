762de8af5b4d054d453bfe2a0d7f9e28fb9c36cb
HarikaPadmini <71378652+gudipudiharika3@users.noreply.github.com>
2025-06-19 17:07:08 -0700

    Unit tests for calculator (#3)
    * Commit changes
    
    * Resolved comments

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-def add(x,y):
+def add(x, y):
     return x+y
-def subtract(x,y):
+
+
+def subtract(x, y):
     return x-y
-def multiply(x,y):
+
+
+def multiply(x, y):
     return x*y
-def divide(x,y):
+
+
+def divide(x, y):
     if y==0:
         return "Error: Division By zero"
     return x/y
+
+
 def calculator():
     print("Simple calculator")
     print("Select options:")
